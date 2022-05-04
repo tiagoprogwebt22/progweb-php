812074d91129753c13e6420bb71a1e719b2eacdf
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-05-04 14:28:39 -0300

    Include e Require
    Exemplos de uso

---
+++ textos.php
@@ -0,0 +1,6 @@
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing 
+elit. Culpa aperiam exercitationem ad expedita dolore hic nostrum consectetur quaerat! Aliquid, ullam repellat? Molestias ducimus dolore numquam ipsam at culpa praesentium ullam.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quam ipsum nisi at debitis incidunt pariatur temporibus voluptas quisquam aliquam.</p>
+
+<?php require "lista.html"; ?>
